9515747e4000b32ed66628e8931cc025d61b3463
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-11-18 20:26:07 +0300

    created contact page to test the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Frontend from './components/frontend'
+import Conctact from './components/caontact'
 
 const App = () => {
   return (
     <div>
       <Frontend/>
+      <Conctact/>
     </div>
   )
 }
